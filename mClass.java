dc3ad00a3e19598f41c779f3b70d9ac529a10207
marktestgit <marktestgit@interia.pl>
2018-12-19 23:13:12 +0100

    ok2

---
+++ mClass.java
@@ -0,0 +1,10 @@
+package hours24;
+
+public class mClass extends sClass{
+
+	public static void main(String[] args) {
+		mClass aa = new mClass();
+		aa.akoli();
+		
+	}
+}
---
+++ sClass.java
@@ -0,0 +1,7 @@
+package hours24;
+
+public class sClass {
+	public void akoli() {
+		System.out.println("ds");
+	}
+}
